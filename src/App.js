3b0a7d42ad530f325e13eca897e95b06e5fd6706
swift1719 <ayushpawar1501@gmail.com>
2021-05-19 00:52:59 +0530

    configured app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Cards,Chart,CountryPicker} from './components';
 import styles from './app';
-import {fetchdata} from './api';
+import {fetchData} from './api';
 import coronaImage from './images/corona.png';
 
 // Component is responsible for sending country & data together into a single view so the website 
@@ -11,5 +11,33 @@ class App extends React.Component{
         data:{},
         country:"",
     };
+    
+    async componentDidMount(){
+        const fetchedData=await fetchData();
+        this.setState({data:fetchedData});
+    }
+    handleCountryChange=async (country)=>{
+        const fetchedData=await fetchData();
+        this.setState({data:fetchedData,country:country});
+    }
+
+    render(){
+        const {data,country}=this.state;
+        return(
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19"/>
+                <br/>
+                <text>
+                    <b>Global and Country Wise COVID-19 cases</b>
+                </text>
+                <br/>
+                <br/>
+                <Cards data={data} country={country}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+            </div>
+        )
+    }
 
 }
+export default App;
